e456a2e5163b2ca83b722fa141f4a44ff077a347
mask619916 <121339077+mask619916@users.noreply.github.com>
2023-12-19 00:06:43 +0300

    added bottom nav bar

--- lib/utilities/tab_bar_navigation_controller.dart
+++ lib/utilities/tab_bar_navigation_controller.dart
@@ -1,11 +1,68 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:cycletech/tab_pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class TabBarNavigationController extends StatelessWidget {
+class TabBarNavigationController extends StatefulWidget {
   const TabBarNavigationController({super.key});
 
   @override
+  State<TabBarNavigationController> createState() =>
+      _TabBarNavigationControllerState();
+}
+
+class _TabBarNavigationControllerState
+    extends State<TabBarNavigationController> {
+  final iconList = <IconData>[
+    Icons.home_outlined,
+    Icons.leaderboard_outlined,
+    Icons.person_outline,
+    Icons.settings_outlined,
+  ];
+
+  int _bottomNavIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const HomePage();
+    return Scaffold(
+      body: Container(),
+      floatingActionButton: SizedBox(
+        height: 75,
+        width: 75,
+        child: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _bottomNavIndex = 4;
+            });
+          },
+          backgroundColor: Colors.greenAccent,
+          splashColor: Colors.cyan[200],
+          shape: const CircleBorder(),
+          child: const FaIcon(FontAwesomeIcons.personBiking),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar.builder(
+        itemCount: iconList.length,
+        backgroundColor: Colors.black54,
+        tabBuilder: (int index, bool isActive) {
+          return Icon(
+            iconList[index],
+            size: 24,
+            color: isActive ? Colors.green[400] : Colors.white,
+          );
+        },
+        activeIndex: _bottomNavIndex,
+        gapLocation: GapLocation.center,
+        notchSmoothness: NotchSmoothness.verySmoothEdge,
+        leftCornerRadius: 32,
+        rightCornerRadius: 32,
+        onTap: (index) {
+          setState(() {
+            _bottomNavIndex = index;
+          });
+        },
+      ),
+    );
   }
 }
