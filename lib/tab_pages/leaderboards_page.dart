6b1e52b29430ae4b92f994f77157e781871ee62b
mask619916 <121339077+mask619916@users.noreply.github.com>
2023-12-26 16:26:44 +0300

    Revert "Revert "leaderboard design finished""
    This reverts commit 19f7fb7795e100b23961ee1f00622a6ea5c19ff8.

--- lib/tab_pages/leaderboards_page.dart
+++ lib/tab_pages/leaderboards_page.dart
@@ -5,13 +5,66 @@ class LeaderboardsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Text("Leaderboards page"),
-          ],
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Leaderboards'),
+          backgroundColor: Colors.blue[100],
+          foregroundColor: Colors.black87,
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Daily'),
+              Tab(text: 'Weekly'),
+              Tab(text: 'All Time'),
+            ],
+          ),
         ),
+        body: SafeArea(
+          child: TabBarView(
+            children: [
+              // Daily Leaderboard Placeholder
+              LeaderboardTab(title: 'Daily'),
+
+              // Weekly Leaderboard Placeholder
+              LeaderboardTab(title: 'Weekly'),
+
+              // All Time Leaderboard Placeholder
+              LeaderboardTab(title: 'All Time'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LeaderboardTab extends StatelessWidget {
+  final String title;
+
+  const LeaderboardTab({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          // Placeholder content for the leaderboard
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text('First Place'),
+          ),
+          ListTile(
+            leading: Icon(Icons.star_border),
+            title: Text('Second Place'),
+          ),
+          ListTile(
+            leading: Icon(Icons.star_border),
+            title: Text('Third Place'),
+          ),
+          // Add more list entries as needed
+          // ...
+        ],
       ),
     );
   }
