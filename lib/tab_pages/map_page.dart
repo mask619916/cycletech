716ae9127e6f2895c207312c25dee120b786e925
TeryakiiSauce <48337702+TeryakiiSauce@users.noreply.github.com>
2023-12-27 05:21:00 +0300

    map works like a charm

--- lib/tab_pages/map_page.dart
+++ lib/tab_pages/map_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
@@ -12,78 +14,102 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  late Position _currentPosition;
-  LatLng _currentLocation = LatLng(0.0, 0.0);
+  final MapController _mapController = MapController();
+  late StreamSubscription<Position> _positionStream;
+  LatLng _locationCords = LatLng(0, 0);
 
-  Future<void> _getCurrentLocation() async {
-    // bool serviceEnabled;
-    // LocationPermission permission;
+  void _initPositionStream() {
+    int distanceFilterInMeters = 50;
+    LocationSettings locationSettings;
 
-    // Check if location services are enabled
-    // serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    // if (!serviceEnabled) {
-    //   // Location services are not enabled, ask user to enable them
-    //   return;
-    // }
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      locationSettings = AndroidSettings(
+        accuracy: LocationAccuracy.bestForNavigation,
+        distanceFilter: distanceFilterInMeters,
+        forceLocationManager: true,
+        intervalDuration: const Duration(seconds: 5),
+        //(Optional) Set foreground notification config to keep the app alive
+        //when going to the background
+        foregroundNotificationConfig: const ForegroundNotificationConfig(
+          notificationText:
+              "CycleTech will continue to receive your location even when you aren't using it",
+          notificationTitle: "Running in Background",
+          enableWakeLock: true,
+        ),
+      );
+    } else if (defaultTargetPlatform == TargetPlatform.iOS ||
+        defaultTargetPlatform == TargetPlatform.macOS) {
+      locationSettings = AppleSettings(
+        accuracy: LocationAccuracy.bestForNavigation,
+        activityType: ActivityType.fitness,
+        distanceFilter: distanceFilterInMeters,
+        pauseLocationUpdatesAutomatically: true,
+        // Only set to true if our app will be started up in the background.
+        showBackgroundLocationIndicator: true,
+      );
+    } else {
+      locationSettings = LocationSettings(
+        accuracy: LocationAccuracy.bestForNavigation,
+        distanceFilter: distanceFilterInMeters,
+      );
+    }
 
-    // Request permission to access location
-    // permission = await Geolocator.checkPermission();
-    // if (permission == LocationPermission.denied) {
-    //   permission = await Geolocator.requestPermission();
-    //   if (permission == LocationPermission.denied) {
-    //     // Permissions are denied, show error or ask user to enable them manually
-    //     return;
-    //   }
-    // }
+    _positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings).listen(
+      (Position? position) {
+        if (position == null) {
+          // print('Location not found');
+          return;
+        }
 
-    // if (permission == LocationPermission.deniedForever) {
-    //   // Permissions are denied forever, handle appropriately.
-    //   return;
-    // }
+        setState(() {
+          _locationCords = LatLng(position.latitude, position.longitude);
+        });
+        // print(_locationCords);
 
-    // Get the current position
-    _currentPosition = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.bestForNavigation,
+        _mapController.move(_locationCords, 16);
+      },
     );
-
-    print("${_currentPosition.latitude}, ${_currentPosition.longitude}");
-
-    setState(() {
-      // Update UI with the current position
-      _currentLocation =
-          LatLng(_currentPosition.latitude, _currentPosition.longitude);
-    });
-
-    print(_currentLocation);
   }
 
   @override
   void initState() {
     super.initState();
-    _getCurrentLocation();
+    // _getCurrentLocation();
+    _initPositionStream();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _positionStream.cancel();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Enjoy your ride!'),
-      ),
+      appBar: AppBar(title: Text('Enjoy your ride!')),
       body: SlidingUpPanel(
         panel: Container(),
         body: Stack(
           children: [
             FlutterMap(
-              mapController: MapController(),
+              mapController: _mapController,
               options: MapOptions(
-                initialCenter: _currentLocation,
+                initialCenter: LatLng(25.948767, 50.567386),
                 initialZoom: 10,
-                
               ),
               children: [
                 TileLayer(
                   urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.cycletech',
                 ),
+                MarkerLayer(markers: [
+                  Marker(
+                    point: _locationCords,
+                    child: FlutterLogo(),
+                  ),
+                ]),
               ],
             ),
           ],
